3ccf673436aaf6ff59172a3c76bc907ec101f627
Chema Tirado <josemariatg98@gmail.com>
2018-12-14 18:00:22 -0700

    contacto-validate

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
@@ -6,58 +6,101 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
-  index: any;
+  index: String;
+
 
   constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   guardarDatos() {
-      const tmpNombreCompleto = <HTMLInputElement> document.getElementById('txtNombreCompleto');
-      const tmpDireccion = <HTMLInputElement> document.getElementById('txtDireccion');
+      const tmpNombre = <HTMLInputElement> document.getElementById('txtNombre');
+      const tmpCorreo = <HTMLInputElement> document.getElementById('txtCorreo');
+      const tmpTelefono = <HTMLInputElement> document.getElementById('txtTelefono');
+      const tmpMensaje = <HTMLInputElement> document.getElementById('txtMensaje');
+      const d: Date = new Date();
+      let validate: Number;
+      let tmp: any;
       const datos = {
-          nombreCompleto: tmpNombreCompleto.value,
-          direccion: tmpDireccion.value,
+          nombre: tmpNombre.value,
+          correo: tmpCorreo.value,
+          telefono: tmpTelefono.value,
+          mensaje: tmpMensaje.value,
+          fecha: d
       };
       if (this.index) {
           // tslint:disable-next-line:no-shadowed-variable
           const http = new XMLHttpRequest();
           http.onreadystatechange = function() {
               if (this.readyState === 4 && this.status === 200) {
-                  // console.log(JSON.parse(this.responseText));
+                console.log(JSON.parse(this.responseText));
               }
           };
-          http.open('PUT', 'https://cielitolindo-377ae.firebaseio.com/datos.json' + this.index + '.json');
-          http.setRequestHeader('Content-Type', 'application/json');
-          http.send(JSON.stringify(datos));
+            http.open('PUT', 'https://cielitolindo-377ae.firebaseio.com/datos.json' + this.index + '.json');
+            http.setRequestHeader('Content-Type', 'application/json');
+            http.send(JSON.stringify(datos));
       } else {
           const http = new XMLHttpRequest();
           http.onreadystatechange = function() {
               if (this.readyState === 4 && this.status === 200) {
-                  // document.getElementById("formulario").reset();
+                console.log('si entro');
+              } else {
+                console.log('no entro');
               }
           };
           http.open('POST', 'https://cielitolindo-377ae.firebaseio.com/datos.json', true);
           http.setRequestHeader('Content-Type', 'application/json');
           http.send(JSON.stringify(datos));
       }
-      alert('Datos guardados');
-      location.href = 'index.html';
+
+      if (validate === 1) {
+        tmp = document.getElementById('mGuardado');
+        tmp.classList.add('invisible');
+        tmp =  document.getElementById('mNoGuardado');
+         tmp.classList.remove('visible');
+      } else if (validate === 2) {
+
+      }
   }
 
     validar() {
-      const tmpNombreCompleto = <HTMLInputElement> document.getElementById('txtNombreCompleto');
-      const tmpDireccion = <HTMLInputElement> document.getElementById('txtNombreCompleto');
-      document.getElementById('txtNombreCompleto').style.border = '2px solid black';
-      document.getElementById('txtDireccion').style.border = '2px solid red';
-      if (tmpNombreCompleto.value === '') {
-          document.getElementById('txtNombreCompleto').style.border = '2px solid red';
-          document.getElementById('txtNombreCompleto').focus();
-      } else if (tmpDireccion.value === '') {
-          document.getElementById('txtDireccion').style.border = '2px solid red';
-          document.getElementById('txtDireccion').focus();
+      const tmpNombre = <HTMLInputElement> document.getElementById('txtNombre');
+      const tmpCorreo = <HTMLInputElement> document.getElementById('txtCorreo');
+      const tmpMensaje = <HTMLInputElement> document.getElementById('txtMensaje');
+      let error;
+      error = document.getElementById('mensajeError');
+      if (tmpMensaje.value === '') {
+          tmpMensaje.classList.add('is-danger');
+          document.getElementById('txtMensaje').focus();
+          error.classList.add('visible');
+          error.classList.remove('invisible');
       } else {
+          tmpMensaje.classList.remove('is-danger');
+          error.classList.add('invisible');
+          error.classList.remove('visible');
+      }
+      error = document.getElementById('correoError');
+      if (tmpCorreo.value === '') {
+          tmpCorreo.classList.add('is-danger');
+          document.getElementById('txtCorreo').focus();
+          error.classList.add('visible');
+          error.classList.remove('invisible');
+      } else {
+          tmpCorreo.classList.remove('is-danger');
+          error.classList.add('invisible');
+          error.classList.remove('visible');
+      }
+      error = document.getElementById('nombreError');
+      if (tmpNombre.value === '') {
+          tmpNombre.classList.add('is-danger');
+          document.getElementById('txtNombre').focus();
+          error.classList.add('visible');
+          error.classList.remove('invisible');
+      } else {
+          tmpNombre.classList.remove('is-danger');
+          error.classList.add('invisible');
+          error.classList.remove('visible');
+      }
+      if (tmpMensaje.value !== '' && tmpCorreo.value !== '' && tmpNombre.value !== '' ) {
           this.guardarDatos();
       }
   }
